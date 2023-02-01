7819f2e7290913cdf1998ab3de99064b47193fb0
Oğuzhan Şenokur <44192576+oguzhansenokur@users.noreply.github.com>
2023-02-01 22:46:25 +0300

    news-homepage re updated

---
+++ components/newshomepage/app.js
@@ -0,0 +1,21 @@
+function openNav() {
+    document.getElementById("mobile-nav").style.width = "70%";
+    document.getElementById("transparent").style.width = "100%";
+    document.body.style.overflowY="hidden"
+
+
+    
+   
+
+
+
+}
+
+function closeNav() {
+    document.getElementById("mobile-nav").style.width = "0";
+    document.getElementById("transparent").style.width = "0";
+    document.body.style.overflowY="unset";
+
+
+
+}
